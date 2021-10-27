5d36a79b6bf1461bd5cb4ba670d576a0ef9d18b4
Christopher Wilson <chris@abitofcode.com>
2021-10-28 00:25:25 +0100

    Working base copy

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 import pkg from "./package.json";
 import { terser } from "rollup-plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs"; // This addressed the jspdf import issue
+import commonjs from "@rollup/plugin-commonjs"; // This addressed the jspdf import issue along with inlineDynamic imports
 
 const name = pkg.name
   .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
@@ -14,6 +14,6 @@ export default {
   input: "src/scripts/main.js",
   //   output: [{ file: pkg.main, format: "iife", name }],
   output: [{ file: pkg.main, format: "esm" }],
-  //plugins: [terser()],
-  plugins: [resolve(), commonjs()],
+  plugins: [terser(), resolve(), commonjs()],
+  //plugins: [resolve(), commonjs()],
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,14 +20,14 @@ export const printButtonClicked = () => {
 
   doc.setFontSize(40);
   doc.text("Octonyan loves jsPDF", 35, 25);
-  doc.addImage(
-    "https://image.digitalcollections.manchester.ac.uk/iiif/VS-VPH-00005-00001-000-00001.jp2/full/,600/0/default.jpg",
-    "JPEG",
-    35,
-    55,
-    120,
-    81
-  );
+  //   doc.addImage(
+  //     "https://image.digitalcollections.manchester.ac.uk/iiif/VS-VPH-00005-00001-000-00001.jp2/full/,600/0/default.jpg",
+  //     "JPEG",
+  //     35,
+  //     55,
+  //     120,
+  //     81
+  //   );
   doc.save("woohoo.pdf");
 };
 
